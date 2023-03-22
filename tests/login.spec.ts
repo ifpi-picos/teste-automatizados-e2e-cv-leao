6e39411e7566a372079e4e9283cae7fc5fa72233
Carlos Victor Leão Almondes <carlosvictorleao@gmail.com>
2023-03-22 19:00:59 -0300

    Teste: Login com usuário inexistente

---
+++ tests/login.spec.ts
@@ -0,0 +1,15 @@
+import { test, expect } from "@playwright/test";
+
+test("Login com usuário inexistente", async ({ page }) => {
+  await page.goto("https://proxima-parada.netlify.app/");
+  await page.goto("https://proxima-parada.netlify.app/#/");
+  await page.goto("https://proxima-parada.netlify.app/#/signin");
+  await page.getByLabel("E-mail").click();
+  await page.getByLabel("E-mail").fill("testeqa@gmail.com");
+  await page.getByLabel("Senha").click();
+  await page.getByLabel("Senha").fill("123456");
+  await page.getByRole("button", { name: "Entrar" }).click();
+  await page.getByRole("alert").click();
+});
+
+test("Logando corretamente", async ({ page }) => {});
