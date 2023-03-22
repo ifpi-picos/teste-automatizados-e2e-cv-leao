e268c5b098161f1bca6fad96e7b3b3417dffb88f
Carlos Victor Leão Almondes <carlosvictorleao@gmail.com>
2023-03-22 19:55:42 -0300

    Teste: Logando corretamente

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -12,4 +12,22 @@ test("Login com usuário inexistente", async ({ page }) => {
   await page.getByRole("alert").click();
 });
 
-test("Logando corretamente", async ({ page }) => {});
+test("Logando corretamente", async ({ page }) => {
+  await page.goto("https://proxima-parada.netlify.app/");
+  await page.goto("https://proxima-parada.netlify.app/#/");
+  await page.goto("https://proxima-parada.netlify.app/#/signin");
+  await page.getByLabel("E-mail").click();
+  await page.getByLabel("E-mail").click();
+  await page.getByLabel("E-mail").fill("cv_user@gmail.com");
+  await page.getByLabel("Senha").click();
+  await page.getByLabel("Senha").fill("123456");
+  await page.getByRole("button", { name: "Entrar" }).click();
+  await page
+    .locator("div")
+    .filter({
+      hasText:
+        "Erro ao conectar-se ao banco de dados!Nenhuma carona encontradaOrigemCidade: Pic",
+    })
+    .nth(2)
+    .click();
+});
