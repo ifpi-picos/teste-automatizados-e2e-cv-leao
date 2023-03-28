3af261ec2dc5cf0c0bbfd47b972f77e470f76d5f
Carlos Victor Leão Almondes <carlosvictorleao@gmail.com>
2023-03-28 16:15:13 -0300

    Teste: E-mail inválido

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -70,3 +70,22 @@ test("Senha incorreta", async ({ page }) => {
   await page.getByRole("alert").click();
   await page.getByRole("alert").click();
 });
+
+test("E-mail inválido", async ({ page }) => {
+  await page.goto("https://proxima-parada.netlify.app/");
+  await page.goto("https://proxima-parada.netlify.app/#/");
+  await page.goto("https://proxima-parada.netlify.app/#/signin");
+  await page.getByLabel("E-mail").click();
+  await page.getByLabel("E-mail").fill("aaaaaaaaa");
+  await page.getByText("E-mail inválido.").click();
+  await page.getByText("E-mail inválido.").click();
+  await page.getByLabel("Senha").click();
+  await page.getByLabel("Senha").fill("123456");
+  await page
+    .getByText(
+      "mail_outlineE-mailE-mail inválido.passwordSenha Entrar Novo no Próxima Parada? C"
+    )
+    .click();
+  await page.getByText("E-mail inválido.").click();
+  await page.getByText("E-mail inválido.").click();
+});
