db6c2d6151b4cbf4e22efb6a4add4c4b31474650
Carlos Victor Leão Almondes <carlosvictorleao@gmail.com>
2023-03-28 20:35:16 -0300

    Teste: Fazer modificações na tela de edição e clicar em cancelar para testar se não modifica algo

--- tests/editar-perfil.spec.ts
+++ tests/editar-perfil.spec.ts
@@ -51,3 +51,31 @@ test("Editar número do WhatsApp", async ({ page }) => {
   await page.getByLabel("Número de Whatsapp").fill("(89) 9 1111-1111");
   await page.getByRole("button", { name: "Salvar" }).click();
 });
+
+test("Fazer modificações na tela de edição e clicar em cancelar para testar se não modifica algo", async ({
+  page,
+}) => {
+  await page.goto("https://proxima-parada.netlify.app/");
+  await page.goto("https://proxima-parada.netlify.app/#/");
+  await page.goto("https://proxima-parada.netlify.app/#/signin");
+  await page.getByLabel("E-mail").click();
+  await page.getByLabel("E-mail").fill("preenchendocorretamente@gmail.com");
+  await page.getByLabel("Senha").click();
+  await page.getByLabel("Senha").fill("123456");
+  await page.getByRole("button", { name: "Entrar" }).click();
+  await page.goto("https://proxima-parada.netlify.app/#/home/profile");
+  await page.getByRole("button", { name: "Editar Perfil" }).click();
+  await page.getByLabel("Nome Completo").click();
+  await page.getByLabel("Nome Completo").press("ArrowRight");
+  await page.getByLabel("Nome Completo").fill("aaaaaaaa");
+  await page.getByLabel("Ocupação no IFPI").click();
+  await page.getByText("Professor(a)").click();
+  await page.getByLabel("Número de Whatsapp").click();
+  await page.getByLabel("Número de Whatsapp").fill("(22) 2 2222-2222");
+  await page.getByRole("button", { name: "Cancelar" }).click();
+  await page
+    .getByText(
+      "Erro ao conectar-se ao banco de dados!Erro ao conectar-se ao banco de dados! aaa"
+    )
+    .click();
+});
